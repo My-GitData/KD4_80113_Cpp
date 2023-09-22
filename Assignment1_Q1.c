b74104ee015f29d04abbe98ae4cc17e1be367760
KD4-80113 <tarungothwal12@gmail.com>
2023-09-22 22:33:24 +0530

    Assignment1

--- Assignment1_Q1.c
+++ Assignment1_Q1.c
@@ -1,44 +1,74 @@
-/*Q1. Write a menu driven program for Date in a C. Declare a structure Date having data members
-day, month, year. Implement the following functions.
-void initDate(struct Date* ptrDate);
-void printDateOnConsole(struct Date* ptrDate);
-void acceptDateFromConsole(struct Date* ptrDate);*/
-#include<stdio.h>
+#include <stdio.h>
+
+// Q1. Write a menu driven program for Date in a C. Declare a structure Date having data members
+// day, month, year. Implement the following functions.
+// void initDate(struct Date* ptrDate);
+// void printDateOnConsole(struct Date* ptrDate);
+// void acceptDateFromConsole(struct Date* ptrDate);
+
 struct Date
 {
-    
-    int day,month,year;
+    int day;
+    int month;
+    int year;
 };
-void initDate(struct Date* ptrDate)
-{
-    ptrDate->day=3;
-    ptrDate->month=3;
-    ptrDate->year=2023;
-    
 
+void initDate(struct Date *d1);
+void acceptDateFromConsole(struct Date *d1);
+void printDateOnConsole(struct Date *d1);
+
+void initDate(struct Date *d1)
+{
+    d1->year = 1900;
+    d1->month = 01;
+    d1->day = 01;
 }
-void acceptDateFromConsole(struct Date *ptrDate)
-{
-    
-    printf("Enter Date : \n");
-    scanf("%d",&ptrDate->day);
-    printf("Enter month : \n");
-    scanf("%d",&ptrDate->month);
-    printf("Enter Date : \n");
-    scanf("%d",&ptrDate->year);
 
-};
-void printDateOnConsole(struct Date* ptrDate)
+void acceptDateFromConsole(struct Date *d1)
 {
-    printf("Date is : %d/%d/%d \n",ptrDate->day,ptrDate->month,ptrDate->year);
+    printf("enter the year=\n");
+    scanf("%d", &d1->year);
+    printf("enter the month=\n");
+    scanf("%d", &d1->month);
+    printf("enter the day=\n");
+    scanf("%d", &d1->day);
 }
-void main()
-{
-    struct Date dat ;
-    
-initDate(&dat);
-printDateOnConsole(&dat);
-acceptDateFromConsole(&dat);
-printDateOnConsole(&dat);
 
+void printDateOnConsole(struct Date *d1)
+{
+    printf("date=%d:%d:%d", d1->year, d1->month, d1->day);
+}
+
+int main()
+{
+    struct Date d1;
+    int x;
+    do
+    {
+        printf("\n select any choice");
+        printf("\n 1.for initalisation   \n 2. for accept from user \n 0.exit \n");
+        scanf("\n %d", &x);
+
+        if (x >= 0 && x < 3)
+        {
+            switch (x)
+            {
+            case 1:
+
+                initDate(&d1);
+                printDateOnConsole(&d1);
+                break;
+
+            case 2:
+                acceptDateFromConsole(&d1);
+                printDateOnConsole(&d1);
+                break;
+            };
+        }
+        else
+            printf("enter valid choice");
+
+    } while (x != 0);
+
+    return 0;
 }
--- Assignment1_Q3.cpp
+++ Assignment1_Q3.cpp
@@ -1,40 +1,85 @@
-/*Q3. Write a menu driven program for Student in CPP language. Create a class student with data
-members roll no, name and marks. Implement the following functions
-void initStudent();
-void printStudentOnConsole();
-void acceptStudentFromConsole();*/
-#include <string>
+// Q3. Write a menu driven program for Student in CPP language. Create a class student with data
+// members roll no, name and marks. Implement the following functions
+// void initStudent();
+// void printStudentOnConsole();
+// void acceptStudentFromConsole();
+
+
+
+#include <iostream>
 using namespace std;
-#include<iostream>
-class Student{
-    int marks,roll_no;
+
+
+//class using cpp
+class Student
+{
+private:
+    int rollno;
     string name;
-    public:
-void initStudent()
-{
-    roll_no=001;
-    name="Sam";
-    marks=90;
-}
-void printfStudentOnConsole()
-{
-    cout<<"Student Roll no :- "<<roll_no<<"\nStudent Name :- "<<name<<"\nStudent Marks :- "<<marks<<"\n";
-}
-void acceptStudentFromConsole()
-{
-cout<<"Enter Student Roll no. :- \n";
-    cin>>roll_no;
-    cout<<"Enter Student Name :- \n";
-    cin>>name;
-    cout<<"Enter Student Marks :- \n";
-    cin>>marks;
-}
+    float marks;
+
+public:
+    void initStudent()
+    {
+        this->rollno = 000;
+        this->name = "krishna";
+        this->marks = 000;
+    }
+
+    void acceptStudentFromConsole()
+    {
+        cout << "enter the rollno=\n"
+             << endl;
+        cin >> this->rollno;
+        cout << "enter the name=\n"
+             << endl;
+        cin >> this->name;
+        cout << "enter the marks=\n"
+             << endl;
+        cin >> this->marks;
+    }
+
+    void printStudentOnConsole()
+    {
+        cout << "stuent details \n";
+        cout << "\n rollno="
+             << this->rollno << "\n name=" << this->name << "\n marks=" << this->marks << endl;
+    }
 };
+
 int main()
 {
-    Student s1;
-    s1.initStudent();
-    s1.printfStudentOnConsole();
-    s1.acceptStudentFromConsole();
-    s1.printfStudentOnConsole();
+    struct Student s1;
+    int x;
+    do
+    {
+        cout << "\n select any choice" << endl;
+        cout << "\n 1.for initalisation   \n 2.accept data from user \n 3.exit "
+             << endl;
+        cin >> x;
+
+        switch (x)
+        {
+        case 1:
+
+            s1.initStudent();
+            s1.printStudentOnConsole();
+            break;
+
+        case 2:
+            s1.acceptStudentFromConsole();
+            s1.printStudentOnConsole();
+            break;
+
+        case 3:
+            break;
+
+        default:
+            cout << "enter valid choice" << endl;
+            break;
+        };
+
+    } while (x != 3);
+
+    return 0;
 }
